3e1cd8f3348a4ac4d64f4f8530180ad4aae290e2
Drew Colthorp <colthorp@atomicobject.com>
2017-10-25 12:00:44 -0400

    Added loading and empty states

--- modules/client/pages/snack-report/snack-report-ui.stories.tsx
+++ modules/client/pages/snack-report/snack-report-ui.stories.tsx
@@ -2,11 +2,19 @@ import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import { SnackReportUI } from "./snack-report-ui";
 
-storiesOf("Page – Snack Report", module).add("Example", () => (
-  <SnackReportUI
-    rows={[
-      { name: "Cheese snack", votes: 8, place: 1, tags: [] },
-      { name: "Guacamole", place: 2, votes: 7, tags: ["Gluten-free", "Vegan"] }
-    ]}
-  />
-));
+storiesOf("Page – Snack Report", module)
+  .add("Example", () => (
+    <SnackReportUI
+      rows={[
+        { name: "Cheese snack", votes: 8, place: 1, tags: [] },
+        {
+          name: "Guacamole",
+          place: 2,
+          votes: 7,
+          tags: ["Gluten-free", "Vegan"]
+        }
+      ]}
+    />
+  ))
+  .add("Empty", () => <SnackReportUI rows={[]} />)
+  .add("Loading", () => <SnackReportUI rows={null} />);
--- modules/client/pages/snack-report/snack-report-ui.tsx
+++ modules/client/pages/snack-report/snack-report-ui.tsx
@@ -8,31 +8,38 @@ export interface SnackReportRow {
 }
 
 export interface SnackReportUIProps {
-  rows: SnackReportRow[];
+  rows: SnackReportRow[] | null;
 }
 
 export const SnackReportUI: React.SFC<SnackReportUIProps> = props => {
-  const rows = props.rows.map((row, i) => (
-    <tr key={i}>
-      <td>{row.place}.</td>
-      <td>{row.votes}</td>
-      <td>{row.name}</td>
-      <td>{row.tags.join(", ")}</td>
-    </tr>
-  ));
-  return (
-    <div className="snack-report">
-      <table>
-        <thead>
-          <tr>
-            <th>Place</th>
-            <th>Votes</th>
-            <th>Snack</th>
-            <th>Tags</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-    </div>
-  );
+  if (props.rows === null) {
+    return <div className="snack-report">Loading...</div>;
+  } else if (props.rows.length === 0) {
+    return <div className="snack-report">No snacks to show.</div>;
+  } else {
+    const rows = props.rows.map((row, i) => (
+      <tr key={i}>
+        <td>{row.place}.</td>
+        <td>{row.votes}</td>
+        <td>{row.name}</td>
+        <td>{row.tags.join(", ")}</td>
+      </tr>
+    ));
+
+    return (
+      <div className="snack-report">
+        <table>
+          <thead>
+            <tr>
+              <th>Place</th>
+              <th>Votes</th>
+              <th>Snack</th>
+              <th>Tags</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    );
+  }
 };
